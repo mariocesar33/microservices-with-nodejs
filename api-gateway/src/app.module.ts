8493e2c3f8201590e00562f894dd2ccf21a29c6d
Mário César Silva <mcgato33@hotmail.com>
2026-08-27 18:03:59 +0100

    Update app.module.ts

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,7 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModules.forRoot({
+      isGlobal: true,
+    }),
+    ThrottleModule.forRoot([
+      {
+        ttl: 60000, // 1 minute
+        limit: 100, // 100 requests por minute
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
