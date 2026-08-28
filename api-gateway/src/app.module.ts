2e6e4ed03a2ac2b4e885870001e351f566368956
Mário César Silva <mcgato33@hotmail.com>
2026-08-28 12:14:39 +0100

    feat: Logging middleware implements

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { 
+  Module,
+  type MiddlewareConsumer,
+  type NestModule,
+ } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ProxyModule } from './proxy/proxy.module';
+import { MiddlewareModule } from './middleware/middleware.module';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
   imports: [
@@ -17,8 +23,14 @@ import { ProxyModule } from './proxy/proxy.module';
       },
     ]),
     ProxyModule,
+    MiddlewareModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
